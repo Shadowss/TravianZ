5adbc57bc9216301c788eaef1ae887e6f3059406
unknown <Idan@Idan-Laptop.(none)>
2012-05-05 01:47:05 +0300

    fix multihunter message

--- GameEngine/Message.php
+++ GameEngine/Message.php
@@ -370,7 +370,7 @@
         		$welcomemsg = preg_replace("'%TIME%'", date("H:i", COMMENCE), $welcomemsg);
         		$welcomemsg = preg_replace("'%PLAYERS%'", $database->countUser(), $welcomemsg);
         		$welcomemsg = preg_replace("'%ALLI%'", $database->countAlli(), $welcomemsg);
-        		return $database->sendMessage($uid, 0, WEL_TOPIC, $welcomemsg, 0);
+        		return $database->sendMessage($uid, 5, WEL_TOPIC, $welcomemsg, 0);
         	}
 
         	private function wordCensor($text) {
