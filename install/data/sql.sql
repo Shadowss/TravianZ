95e5f437dab46ef559be7513ea20ecfb6e36bf65
unknown <Idan@Idan-Laptop.(none)>
2012-07-15 02:01:55 +0300

    more update

--- install/data/sql.sql
+++ install/data/sql.sql
@@ -188,6 +188,7 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%artefacts` (
   `desc` text NOT NULL,
   `effect` varchar(100) NOT NULL,
   `img` varchar(20) NOT NULL,
+  `active` tinyint(1) unsigned NOT NULL,
   `kind` tinyint(1) unsigned NOT NULL,
   `bad_effect` tinyint(1) unsigned NOT NULL,
   `effect2` tinyint(2) unsigned NOT NULL,
