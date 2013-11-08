b30654037577214e3c7bb067944932ca3b546310
Shadow <cata7007@gmail.com>
2013-11-08 09:52:31 +0200

    Update Session.php

--- GameEngine/Session.php
+++ GameEngine/Session.php
@@ -36,7 +36,6 @@ include ("Form.php");
 include ("Generator.php");
 include ("Multisort.php");
 include ("Ranking.php");
-include ("Automation.php");
 include ("Lang/" . LANG . ".php");
 include ("Logging.php");
 include ("Message.php");
