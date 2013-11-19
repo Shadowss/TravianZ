c93981fadc6399927e5cbefb5de906e222590445
Shadow <cata7007@gmail.com>
2013-11-19 12:16:49 +0200

    Update db_MYSQL.php

--- GameEngine/Database/db_MYSQL.php
+++ GameEngine/Database/db_MYSQL.php
@@ -2717,11 +2717,11 @@ class MYSQL_DB {
 		return mysql_fetch_assoc($result);
 	}
 	
-	function getOasisEnforce($ref, $mode=0) {
+    	function getOasisEnforce($ref, $mode=0) {
         if (!$mode) {
-            	$q = "SELECT e.*,o.conqured FROM ".TB_PREFIX."enforcement as e LEFT JOIN ".TB_PREFIX."odata as o ON e.vref=o.wref where o.conqured = $ref AND e.from !=$ref";
+            $q = "SELECT e.*,o.conqured FROM ".TB_PREFIX."enforcement as e LEFT JOIN ".TB_PREFIX."odata as o ON e.vref=o.wref where o.conqured = $ref AND e.from !=$ref";
         }else{
-            $q = "SELECT e.*,o.conqured FROM ".TB_PREFIX."enforcement as e LEFT JOIN ".TB_PREFIX."odata as o ON e.from=o.wref where o.conqured = $ref AND e.vref !=$ref";
+            $q = "SELECT e.*,o.conqured FROM ".TB_PREFIX."enforcement as e LEFT JOIN ".TB_PREFIX."odata as o ON e.vref=o.wref where o.conqured = $ref";
         }
         $result = mysql_query($q, $this->connection);
         return $this->mysql_fetch_all($result);
