e50a8a905a49d690cbc4a5c593e03228059dff90
Shadow <cata7007@gmail.com>
2014-03-12 08:35:19 +0200

    Update sql.sql

--- install/data/sql.sql
+++ install/data/sql.sql
@@ -1501,6 +1501,9 @@ CREATE TABLE IF NOT EXISTS `%PREFIX%users` (
   `friend19wait` int(11) unsigned NOT NULL,
   `maxevasion` mediumint(3) unsigned NOT NULL,
   `village_select` bigint(20) DEFAULT NULL,
+  `vac_time` varchar(255) NOT NULL,
+  `vac_mode` int(2) unsigned NOT NULL DEFAULT '0',
+  `vactwoweeks` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
