2bf923020d864a3df5d7d24c78a955d6e1b1f114
Shadow <cata7007@gmail.com>
2014-12-02 09:32:11 +0200

    Resetting Game Server

---
+++ Admin/Templates/resetdone.php
@@ -0,0 +1,27 @@
+<?php
+#################################################################################
+##              -= YOU MAY NOT REMOVE OR CHANGE THIS NOTICE =-                 ##
+## --------------------------------------------------------------------------- ##
+##  Filename       resetdone.tpl                                               ##
+##  Developed by:  Ronix                                                       ##
+##  License:       TravianZ Project                                            ##
+##  Copyright:     TravianZ (c) 2012-2014. All rights reserved.                ##
+##                                                                             ##
+#################################################################################
+unset($_SESSION['username'], $_SESSION['sessid']);
+?>
+<table id="member">
+    <thead>
+        <tr>
+            <th>Server Resetting</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <br>
+                <center>Resetting Server already done.</center><br>Multihunter password is: 123456<br>&nbsp;
+            </td>
+        </tr>
+    </tbody>
+</table>
