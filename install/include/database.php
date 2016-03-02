248aa1f2a4ae5fc51fdbc1bb9306200366d78df6
Shadow <cata7007@gmail.com>
2016-03-02 07:50:37 +0200

    Update database.php

--- install/include/database.php
+++ install/include/database.php
@@ -27,7 +27,7 @@ class MYSQL_DB {
 	var $connection;
 
 	function MYSQL_DB() {
-		$this->connection = mysql_connect(SQL_SERVER, SQL_USER, SQL_PASS) or die(mysql_error());
+		$this->connection = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASS) or die(mysql_error());
 		mysql_select_db(SQL_DB, $this->connection) or die(mysql_error());
 	}
 
