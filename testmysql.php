7fe285da2ead95112ff6497fd133da5a078f2a73
uroskn <uros@knuples.net>
2016-07-03 13:53:28 +0200

    Removed testmysql.php script.
    It does not appear to be used.

--- testmysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysql_connect('hostname','dbuser','dbpassword');
-if (!$link) {
-	die('Could not connect to MySQL: ' . mysql_error());
-}
-echo 'Connection OK'; mysql_close($link);
-?>
