9a3aff2efe0a2083ac904800ddde57fcc8dccb22
Martin Ambrus <martin@martinambrus.com>
2017-08-28 22:22:37 +0200

    fix: MySQLi in MultiHunter installation step works now :P

--- install/include/multihunter.php
+++ install/include/multihunter.php
@@ -6,12 +6,12 @@ $gameinstall = 1;
 		include ("../../GameEngine/Admin/database.php");
 		include ("../../GameEngine/Lang/" . LANG . ".php");
 
-		mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASS);
-		mysqli_select_db(SQL_DB);
+		$conn = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASS);
+		mysqli_select_db($conn, SQL_DB);
 
 		if(isset($_POST['mhpw'])) {
 			$password = $_POST['mhpw'];
-			mysqli_query("UPDATE " . TB_PREFIX . "users SET password = '" . md5($password) . "' WHERE username = 'Multihunter'");
+			mysqli_query($conn, "UPDATE " . TB_PREFIX . "users SET password = '" . md5($password) . "' WHERE username = 'Multihunter'");
 			$wid = $admin->getWref(0, 0);
 			$uid = 5;
 			$status = $database->getVillageState($wid);
