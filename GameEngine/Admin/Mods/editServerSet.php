4788e882dffa3ec7a402b7c515ed063830be48a2
Martin Ambrus <martin@martinambrus.com>
2017-09-19 21:25:36 +0200

    fix: database file did not have config included in some cases

--- GameEngine/Admin/Mods/editServerSet.php
+++ GameEngine/Admin/Mods/editServerSet.php
@@ -11,7 +11,6 @@
 
 if(!isset($_SESSION)) session_start();
 if($_SESSION['access'] < 9) die(ACCESS_DENIED_ADMIN);
-include_once("../../config.php");
 include_once("../../Database.php");
 $id = $_POST['id'];
 
