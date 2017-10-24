961a11d16ac1ed6d57e4b8b46b827311cdf4f35e
Martin Ambrus <martin@martinambrus.com>
2017-10-24 21:29:18 +0200

    fix: raidable admin in installation script configured properly

--- install/process.php
+++ install/process.php
@@ -66,6 +66,7 @@ class Process {
 		$text = preg_replace("'%AEMAIL%'", $_POST['aemail'], $text);
 		$text = preg_replace("'%ANAME%'", $_POST['aname'], $text);
 		$text = preg_replace("'%ASUPPMSGS%'", ($_POST['admin_support_msgs'] == 'True' ? 'true' : 'false'), $text);
+		$text = preg_replace("'%ARAIDS%'", ($_POST['admin_raidable'] == 'True' ? 'true' : 'false'), $text);
 		//$text = preg_replace("'%SUBDOM%'", $_POST['subdom'], $text);
 		$text = preg_replace("'%LOGBUILD%'", $_POST['log_build'], $text);
 		$text = preg_replace("'%LOGTECH%'", $_POST['log_tech'], $text);
