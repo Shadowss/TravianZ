ca2a823be19dfa7c339218581cf8444215215824
Shadow <1140613+Shadowss@users.noreply.github.com>
2017-11-10 10:55:32 +0200

    Code clear
    No need , double troops not appear anymore

--- GameEngine/Technology.php
+++ GameEngine/Technology.php
@@ -165,21 +165,6 @@ class Technology {
 
 	public function getUnitList() {
 		global $database,$village;
-	// FIX BY MisterX
-	/*
-  	$controlloTruppeInRinforzo = $database->getEnforceControllTroops($village->wid);
-     	for($i=1;$i<=50;$i++) {
-     	if($controlloTruppeInRinforzo['u'.$i] >= "30000000")
-     	mysqli_query("UPDATE ".TB_PREFIX."enforcement set u".$i." = '0' where vref = $village->wid");
-     	}
-	//FIX BY Shadow and made by NIKO28
-	$controlloTruppe = $database->getUnit($village->wid);
-   	for($i=1;$i<=50;$i++) {
-   	if($controlloTruppe['u'.$i] >= "10000000")
-   	mysqli_query("UPDATE ".TB_PREFIX."units set u".$i." = '0' where vref = $village->wid");
-   	}
-	*/
-	// END FIX
 		$unitarray = func_num_args() == 1? $database->getUnit(func_get_arg(0)) : $village->unitall;
 		$listArray = array();
 		for($i=1;$i<count($this->unarray);$i++) {
