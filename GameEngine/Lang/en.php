8d7da83a6fb3ae722187deb88e774b2d0b485e2a
Shadow <1140613+Shadowss@users.noreply.github.com>
2018-05-11 11:19:44 +0300

    Update message for WW Village Release
    @iopietro must implement that text in separate messages.

--- GameEngine/Lang/en.php
+++ GameEngine/Lang/en.php
@@ -1210,6 +1210,32 @@ The construction plans are conquerable immediately when they appear to the serve
 
 There will be a countdown in game, showing the exact time of the release, 5 days prior to the launch. ");
 
+//WW Village Release Message
+define("WWVILLAGE","Wonder of World Village
+
+Countless days have passed since the first battles upon the walls of the cursed villages of the Dread Natars, many armies of both the free ones and the Natarian empire struggled and died before the walls of the many strongholds from which the Natars had once ruled all creation. Now with the dust settled and a relative calm having settled in, armies began to count their losses and collect their dead, the stench of combat still lingering in the night air, a smell of a slaughter unforgettable in its extent and brutality yet soon to be dwarfed by yet others. The largest armies of the free ones and the Dread Natars were marshalling for yet another renewed assault upon the coveted former strongholds of the Natarian Empire.
+Soon scouts arrived telling of a most awesome sight and a chilling reminder, a dread army of an unfathomable size had been spotted marshalling at the end of the world, the Natarian capital, a force so great and unstoppable that the dust from their march would choke off all light, a force so brutal and ruthless that it would crush all hope. The free people knew that they had to race now, race against time and the endless hordes of the Natarian Empire to raise a Wonder of the World to restore the world to peace and vanquish the Natarian threat.
+But to raise such a great Wonder would be no easy task, one would need construction plans created in the distant past, plans of such an arcane nature that even the very wisest of sages knew not their contents or locations.
+Tens of thousands of scouts roamed across all existence searching in vain for these mystical plans, looking in all places but the dreaded Natarian Capital, yet could not find them. Today however, they return bearing good news, they return baring the locations of the plans, hidden by the armies of the Natars inside secret strongholds constructed to be hidden from the eyes of man.
+Now begins the final stretch, when the greatest armies of the Free people and the Natars will clash across the world for the fate of all that lies under heaven. This is the war that will echo across the eons, this is your war, and here you shall etch your name across history, here you shall become legend.
+
+<img src=".\gpack\travian_default\img\newbackground_messages.png" alt="WWVILLAGE">
+
+To conquer one, the following things must happen:
+
+1. You must attack the village (NO Raid!)
+2. WIN the Attack
+3. Destroy the RESIDENCE
+4. You must decrease the loyalty to 0 with : SENATORS , CHIEF , CHIEFTAIN
+5. You must have enough culture points to conquer the village
+
+If not, the next attack on that village, winning with a SENATORS , CHIEF , CHIEFTAIN and empty slots in RESIDENCE/PALACE will take the village.
+
+To build a WW, you must own a plan yourself (you = the WW village owner) from lvl 0 to 50, from 51 to 100 you need an additional plan in your alliance! Two plans in the WW village account would not work!
+
+The construction plans are conquerable immediately when they appear to the server. 
+
+There will be a countdown in game, showing the exact time of the release, 5 days prior to the launch. ");
 
 //Building Plans
 define("PLAN","Ancient Construction Plan");
