54e65e59c49ebf7e01137c8467e5dc15f8905177
221V <221v92@gmail.com>
2024-12-11 16:22:39 +0200

    tr - css - artefact

--- GameEngine/Lang/zh_tw.php
+++ GameEngine/Lang/zh_tw.php
@@ -1306,7 +1306,7 @@ define("ARTEFACT","<h1><b>Natars Artefacts</b></h1>
 
 Whispering rumors echo through the villages, sharing legends told only by the best storytellers. It refers to NATARS, the most feared warrior of the TRAVIAN world. Their killing is the dream of any hero, the purpose of any fighter. No one knows how NATARS got to get such power, and their warriors so cruel. Determined to discover the source of the NATARS power, the fighters send a group of elite spies to spy them. I do not go through many hours and come back with fear in their eyes and balancing fantastic theories: it seems that the natural power comes from the mysterious objects they call artefacts that they stole from our ancestors. Try to steal the artefacts of her, and you can control their power.
 
-<img src=\"img/x.gif\" class=\"ArtifactsAnnouncement\">
+<img src=\"img/x.gif\" class=\"ArtefactsAnnouncement\">
 
 The time has come for claiming artefacts. Collaborate with your alliance and bring your worriors to get these wanted objects. However, NATARS will not give up without war to the artefacts ... nor your enemies. If you are successful in retrieving artefacts and you will be able to reject enemies, you will be able to collect the rewards. Your buildings will become incredibly strong and mightest, and the troops will be much faster and will consume less food. Capture the artefacts, bring glory over your empire and become new legends for your followers.
 
